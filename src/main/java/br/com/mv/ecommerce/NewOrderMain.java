94c5d2fbc8ec7a7c05fabf6dd66a70691384c9ed
mateusValentim <mateus.valentim19@hotmail.com>
2023-07-30 13:35:58 -0300

    create producers

---
+++ src/main/java/br/com/mv/ecommerce/NewOrderMain.java
@@ -0,0 +1,36 @@
+package br.com.mv.ecommerce;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+        var sales = new HashMap<>();
+        sales.put("iPhone14Pro","14000");
+        sales.put("MacBook Pro 256GB","27000");
+        //var value = "123456, 454872, 78964, 96578";
+        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", sales.toString());
+        producer.send(record, (metadata, exception) -> {
+            if (exception != null){
+                exception.printStackTrace();
+                return;
+            }
+            System.out.println("Success! sending message " + metadata.topic() + ":::partition" + metadata.partition() + "/offset " + metadata.offset() + "/ timestamp" + metadata.timestamp());
+        }).get();
+    }
+
+    private static Properties properties(){
+        var prop = new Properties();
+        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return prop;
+    }
+    }
