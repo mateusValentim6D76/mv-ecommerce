3937ba69dc60a374a0b796436d100da7b5e2a40f
mateusValentim <mateus.valentim19@hotmail.com>
2023-08-02 09:29:04 -0300

    Paralelizando e inserindo Keys

--- src/main/java/br/com/mv/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/mv/ecommerce/FraudDetectorService.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.UUID;
 
 public class FraudDetectorService {
 
@@ -40,7 +41,8 @@ public class FraudDetectorService {
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, StringDeserializer.class.getSimpleName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() +" "+UUID.randomUUID().toString());
         return properties;
     }
 }
--- src/main/java/br/com/mv/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/mv/ecommerce/NewOrderMain.java
@@ -8,20 +8,30 @@ import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.HashMap;
 import java.util.Properties;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
-        var sales = new HashMap<>();
-        //sales.put("iPhone11Pro","4000");
-        //sales.put("MacBook Pro 256GB","27000");
-        var value = "123456, 454872, 78964, 96578";
-        var email = "Thank you for your order! We are processing your order!";
-        var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
-        var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", email, email);
-        producer.send(record, getCallback()).get();
-        producer.send(emailRecord, getCallback()).get();
+    public static <producer> void main(String[] args) throws ExecutionException, InterruptedException {
+        long startTime = System.currentTimeMillis();
+        var numInterations = 100;
+        for (int i = 0; i < numInterations; i++) {
+            var key = UUID.randomUUID().toString();
+            var producer = new KafkaProducer<String, String>(properties());
+            //var sales = new HashMap<>();
+            //sales.put("iPhone11Pro","4000");
+            //sales.put("MacBook Pro 256GB","27000");
+            var value = "123456, 454872, 78964, 96578";
+            var email = "Thank you for your order! We are processing your order!";
+            var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", key, value);
+            var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", key, email);
+            producer.send(record, getCallback()).get();
+            producer.send(emailRecord, getCallback()).get();
+        }
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+
+        System.out.println("Tempo de execução: " + executionTime + " milissegundos");
     }
 
     private static Callback getCallback() {
@@ -34,11 +44,11 @@ public class NewOrderMain {
         };
     }
 
-    private static Properties properties(){
+    private static Properties properties() {
         var prop = new Properties();
         prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return prop;
     }
-    }
+}
