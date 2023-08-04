579a7332d7dbcba624f0fff004579eb634b1dd35
mateusValentim <mateus.valentim19@hotmail.com>
2023-08-03 23:39:15 -0300

    Extraindo uma camada de consumidor

---
+++ src/main/java/br/com/mv/ecommerce/ConsumerFraudDetectorService.java
@@ -0,0 +1,28 @@
+package br.com.mv.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public class ConsumerFraudDetectorService {
+
+    public static void main(String[] args) throws InterruptedException {
+        var fraudDetectorService = new ConsumerFraudDetectorService();
+        var service = new KafkaService(ConsumerFraudDetectorService.class.getSimpleName(),"ECOMMERCE_NEW_ORDER", fraudDetectorService::parse);
+        service.run();
+    }
+
+        private void parse (ConsumerRecord < String, String > record){
+            System.out.println("----------------------------");
+            System.out.println("Processing new order, checking fraud");
+            System.out.println(record.key());
+            System.out.println(record.value());
+            System.out.println("partition::: " + record.partition());
+            System.out.println("offset::: " + record.offset());
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                //ignore
+                e.printStackTrace();
+            }
+            System.out.println("Order processed");
+        }
+}
--- src/main/java/br/com/mv/ecommerce/ProducerNewOrderMain.java
+++ src/main/java/br/com/mv/ecommerce/ProducerNewOrderMain.java
@@ -6,24 +6,21 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.HashMap;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
-public class NewOrderMain {
-    public static <producer> void main(String[] args) throws ExecutionException, InterruptedException {
+public class ProducerNewOrderMain {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
         long startTime = System.currentTimeMillis();
+
         for (int i = 0; i < 10; i++) {
             var key = UUID.randomUUID().toString();
-            var producer = new KafkaProducer<String, String>(properties());
-            //var sales = new HashMap<>();
-            //sales.put("iPhone11Pro","4000");
-            //sales.put("MacBook Pro 256GB","27000");
-            var value = "123456, 454872, 78964, 96578";
+            var value = key + ",123456,454872";
             var email = "Thank you for your order! We are processing your order!";
-            var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", key, value);
-            var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", key, email);
+            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
             producer.send(record, getCallback()).get();
             producer.send(emailRecord, getCallback()).get();
         }
@@ -32,7 +29,6 @@ public class NewOrderMain {
 
         System.out.println("Tempo de execução: " + executionTime + " milissegundos");
     }
-
     private static Callback getCallback() {
         return (metadata, exception) -> {
             if (exception != null) {
@@ -42,7 +38,6 @@ public class NewOrderMain {
             System.out.println("Success! sending message " + metadata.topic() + ":::partition" + metadata.partition() + "/offset " + metadata.offset() + "/ timestamp" + metadata.timestamp());
         };
     }
-
     private static Properties properties() {
         var prop = new Properties();
         prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
