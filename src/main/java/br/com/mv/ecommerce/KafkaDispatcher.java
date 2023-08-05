4c17b036386f49209685fcfd7869f0691ed93aa4
mateusValentim <mateus.valentim19@hotmail.com>
2023-08-05 11:47:48 -0300

    Extraindo a camada de Producer

---
+++ src/main/java/br/com/mv/ecommerce/KafkaDispatcher.java
@@ -0,0 +1,43 @@
+package br.com.mv.ecommerce;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+class KafkaDispatcher {
+
+    private final KafkaProducer<String, String> producer;
+
+    KafkaDispatcher(){
+    this.producer = new KafkaProducer<>(properties());
+    }
+
+    private static Properties properties() {
+        var prop = new Properties();
+        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return prop;
+    }
+
+     void send(String topic, String key, String value) throws ExecutionException, InterruptedException {
+         var record = new ProducerRecord<>(topic, key, value);
+         producer.send(record, getCallback()).get();
+     }
+
+     private static Callback getCallback() {
+         return (metadata, exception) -> {
+             if (exception != null) {
+                 exception.printStackTrace();
+                 return;
+             }
+             System.out.println("Success! sending message " + metadata.topic() + ":::partition" + metadata.partition() + "/offset " + metadata.offset() + "/ timestamp" + metadata.timestamp());
+         };
+     }
+
+ }
--- src/main/java/br/com/mv/ecommerce/ProducerNewOrderMain.java
+++ src/main/java/br/com/mv/ecommerce/ProducerNewOrderMain.java
@@ -12,37 +12,19 @@ import java.util.concurrent.ExecutionException;
 
 public class ProducerNewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
+        var dispatcher = new KafkaDispatcher();
         long startTime = System.currentTimeMillis();
 
         for (int i = 0; i < 10; i++) {
             var key = UUID.randomUUID().toString();
             var value = key + ",123456,454872";
+            dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
             var email = "Thank you for your order! We are processing your order!";
-            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
-            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
-            producer.send(record, getCallback()).get();
-            producer.send(emailRecord, getCallback()).get();
+            dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
         }
+
         long endTime = System.currentTimeMillis();
         long executionTime = endTime - startTime;
-
         System.out.println("Tempo de execução: " + executionTime + " milissegundos");
-    }
-    private static Callback getCallback() {
-        return (metadata, exception) -> {
-            if (exception != null) {
-                exception.printStackTrace();
-                return;
-            }
-            System.out.println("Success! sending message " + metadata.topic() + ":::partition" + metadata.partition() + "/offset " + metadata.offset() + "/ timestamp" + metadata.timestamp());
-        };
-    }
-    private static Properties properties() {
-        var prop = new Properties();
-        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        return prop;
     }
 }
